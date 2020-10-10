fe24d9496d8e1c069222748a1d28a0eb82f210ac
Frans Allen <git.a@upset.dev>
2020-10-11 03:47:00 +0700

    Smaller title for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import Layout from '../layouts/default';
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
+    <h2>NOT FOUND</h2>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 );
