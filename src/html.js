c0216132d3ff1055e97ba1eba3eb7fd276a24928
Frans Allen <fransallen@users.noreply.github.com>
2021-12-13 16:11:53 +0700

    dns prefetch and preconnect for Google Fonts

--- src/html.js
+++ src/html.js
@@ -8,6 +8,8 @@ export default function HTML(props) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="dns-prefetch" href="//cdn.statically.io" />
+        <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&amp;display=swap"
