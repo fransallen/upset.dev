f7b1eb698c6f27d856dedf24eb077cc41cf22fda
Frans Allen <fransallen@users.noreply.github.com>
2023-07-10 04:33:54 +0700

    added more redirections

--- next.config.js
+++ next.config.js
@@ -8,8 +8,68 @@ const nextConfig = {
         permanent: true,
       },
       {
+        source: "/statically",
+        destination: "https://statically.io",
+        permanent: true,
+      },
+      {
+        source: "/puredns",
+        destination: "https://puredns.org",
+        permanent: true,
+      },
+      {
+        source: "/favicone",
+        destination: "https://favicone.com",
+        permanent: true,
+      },
+      {
+        source: "/blobcdn",
+        destination: "https://blobcdn.com",
+        permanent: true,
+      },
+      {
         source: "/indiwtf",
         destination: "https://indiwtf.upset.dev",
+        permanent: true,
+      },
+      {
+        source: "/fonts",
+        destination: "https://fonts.upset.dev",
+        permanent: true,
+      },
+      {
+        source: "/httpcheck",
+        destination: "https://httpcheck.upset.dev",
+        permanent: true,
+      },
+      {
+        source: "/thedevid",
+        destination: "https://thedev.id",
+        permanent: true,
+      },
+      {
+        source: "/facemash",
+        destination: "https://facemash.upset.dev",
+        permanent: true,
+      },
+      {
+        source: "/kominfu",
+        destination: "https://kominfu.com",
+        permanent: true,
+      },
+      {
+        source: "/indiwtf-cli",
+        destination: "https://github.com/fransallen/indiwtf-cli",
+        permanent: true,
+      },
+      {
+        source: "/indiwtf-api",
+        destination: "https://indiwtf.upset.dev/api",
+        permanent: true,
+      },
+      {
+        source: "/easyrpz",
+        destination: "https://github.com/pure-dns/easyrpz",
         permanent: true,
       },
       {
