755df3a2d5fdada2df25fbffd682fb16caf2d306
Frans Allen <fransallen@users.noreply.github.com>
2023-10-11 08:47:18 +0700

    added github chart

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,18 @@ export default function Index() {
       </article>
 
       <FeaturedProjects />
+
+      <section id="stats" className="max-w-4xl mx-auto p-6 lg:pt-10 lg:px-8">
+        <h2 className="text-2xl font-bold text-center text-black/90">Stats</h2>
+        <img
+          src="https://ghchart.rshah.org/fransallen"
+          alt="Frans Allen's GitHub Chart"
+          width={600}
+          height={100}
+          className="mx-auto mt-8"
+        />
+      </section>
+
       <Press />
       <Podcasts />
       <Coupons />
